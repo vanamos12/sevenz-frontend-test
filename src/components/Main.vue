a66f8127a6d7d69c95e758f04febcd43d8f1563f
Gediser Douala <gediserdouala2021@gmail.com>
2022-07-27 17:46:09 +0200

    first stage of main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,7 +8,10 @@
                 <img alt="profile" src="./../assets/profile.png" />
             </div>
         </div>
-        
+        <div class="row-title">
+            <h1>Update Patient Medical Record</h1>
+            <h2>Click the tabs to view and edit patient medical details</h2>
+        </div>
     </div>
 </template>
 
@@ -23,11 +26,50 @@ export default {
 #main{
     width: 900px;
     background-color: var(--bg-main-color);
+    padding: 0 2rem;
 }
-
+.row-right{
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
 #header{
     display: flex;
     align-items: center;
-    gap: 1rem;
+    gap: 2rem;
+}
+
+h3{
+    color: var(--text-h3-color);
+    font-size: 18px;
+
+}
+
+img{
+    width: 50px;
+    border-radius: 50%;
+}
+
+i{
+    transform: scale(1.5);
+}
+
+i.bxs-bell{
+    color: var(--text-h3-color);
+}
+
+i.bxs-notification{
+    color: var(--bg-side-menu-color);
+}
+
+h1{
+    font-size: 25px;
+    font-weight: 900;
+    color: var(--bg-side-menu-color);
+}
+
+h2{
+    font-size: 12px;
+    color: var(--text-h2-color);
 }
 </style>
