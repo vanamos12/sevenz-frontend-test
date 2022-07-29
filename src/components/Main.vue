7c7eae5ab11783c4a0ce4b6aabae8335642e5bd4
vanamos12 <pokatchoneng@yahoo.fr>
2022-07-29 11:01:15 +0100

    Presque a la fin du main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -47,14 +47,21 @@
             </div>
         </div>
     </div>
+    <PopUp 
+        v-if="message != ''" 
+        :message="message"
+        @close="close"/>
 </template>
 
 <script>
 import axiosClient from '../axios'
 import store from '../store'
+import PopUp from './PopUp.vue';
 
 export default {
-    components:{},
+    components:{
+        PopUp,
+    },
     mounted(){
         var self=this;
         let bodyFormData = new FormData();
@@ -84,6 +91,7 @@ export default {
     },
     methods:{
         save(){
+            var self =this;
             let bodyFormData = new FormData(document.getElementById('form'))
             bodyFormData.append("developer", "Developer");
             axiosClient.post('/investigations', bodyFormData, {
@@ -95,16 +103,22 @@ export default {
             })
             .then(function (response) {
                 console.log("post investigations success", response)
+                self.message = "Data Sucessfully Registered.";
             })
             .catch(error => {
                 console.log("post investigations error", error);
+                self.message = "An error occured."
             })
 
+        },
+        close(){
+            this.message = '';
         }
     },
     data(){
         return {
             data:[],
+            message:''
         }
     }
 }
@@ -115,6 +129,7 @@ export default {
     width: 900px;
     background-color: var(--bg-main-color);
     padding: 0 2rem;
+    padding-bottom: 2rem;
 }
 .row-right{
     display: flex;
@@ -160,4 +175,72 @@ h2{
     font-size: 12px;
     color: var(--text-h2-color);
 }
+
+.input-list p{
+    color: var(--bg-side-menu-color);
+    font-size: 17px;
+    font-weight: bold;
+}
+
+#form{
+    padding: 1.5rem;
+    background-color: white;
+}
+
+.input-list .investigations{
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 1rem;
+    padding-bottom: 1rem;
+}
+
+.input-list .investigations label{
+    font-weight: bold;
+}
+
+.submit-button{
+    display: flex;
+    justify-content: flex-end;
+}
+
+.submit-button .btn{
+    color: white;
+    background-color: var(--bg-side-menu-color);
+    padding: 10px;
+    font-weight: bold;
+}
+
+hr{
+    margin-bottom: 1.5rem;
+}
+
+.select-list{
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    padding-bottom: 1.5rem;
+}
+
+.select-list .select-box{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.5rem;
+    color: var(--text-side-color);
+     width: 50%;
+}
+
+.select-box label{
+    font-size: 13px;
+}
+
+.select-box select{
+    border: 2px solid var(--text-side-color);
+    padding: 0.5rem;
+    padding-right: 2rem;
+    color: var(--text-side-color);
+    width: 100%;
+    border-radius: 5px;
+}
+
 </style>
---
+++ src/components/PopUp.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="wrapper-pop-up">
+        <div class="main">
+            <div class="header">
+                <h2>Infos</h2>
+            </div>
+            <p>{{message}}</p> 
+            <div class="submit-button"> 
+                <button class="btn" @click.prevent="$emit('close')">Close</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        message: String
+    }
+}
+</script>
+
+<style scoped>
+    .wrapper-pop-up{
+        width: 100vm;
+        height: 100vh;
+        background-color: rgba(157, 150, 150, 0.4);
+        z-index: 10;
+        display: grid;
+        place-items: center;
+    }
+
+    .header h2{
+        color: var(--text-h3-color);
+        font-size: 18px;
+        font-weight: bold;
+    }
+
+    .header, .main p{
+        margin-bottom: 1rem;
+    }
+
+    .main{
+        width: 400px;
+        height: 400px;
+        padding: 1rem;
+        background-color: var(--bg-main-color);
+    }
+
+    .submit-button{
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+
+    .submit-button .btn{
+        color: white;
+        background-color: var(--bg-side-menu-color);
+        padding: 10px;
+        font-weight: bold;
+    }
+</style>
